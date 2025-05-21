003d04ac38a7b09cb00f4e2d16f5942e59dcd988
riccardociccarell1 <riccardo.ciccarelli@studenti.unicam.it>
2025-05-21 14:59:01 +0200

    Aggiunta Springboot

---
+++ src/main/java/it/unicam/cs/ids25/model/Filiera.java
@@ -0,0 +1,14 @@
+package it.unicam.cs.ids25.model;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Filiera {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Filiera.class, args);
+    }
+
+}
